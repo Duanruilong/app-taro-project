bd3d9ac930af26141f37842615541ea494e1dd0b
Drlong <drl1210@163.com>
2023-08-18 10:06:46 +0800

    SearchPage-list

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -2,141 +2,130 @@
  * @Author: duanruilong
  * @Date: 2022-07-22 17:25:19
  * @LastEditors: Drlong drl1210@163.com
- * @LastEditTime: 2023-08-18 09:44:29
+ * @LastEditTime: 2023-08-18 10:06:26
  * @Description: 政策列表
  */
 import { useState, useRef, useEffect } from "react";
 import Taro from "@tarojs/taro";
-import { View, Button, Image } from "@tarojs/components";
-import YListView from "@/components/YListView";
-import YInputSearch from "@/components/YInputSearch";
+import { View, Image } from "@tarojs/components";
+import TMask from "@/components/tinker/TMask";
 import YNoData from "@/components/YNoData";
-import YButton from "@/components/YButton";
-import listItemImg from "@/assets/index_list1.png";
+import YListView from "@/components/YListView";
 import { getStorageData, isEmpty } from "@/utils/utils";
-import { toast } from "@/utils/tools";
-import { getList, getFollow } from "./service";
+import { getList } from "./service";
 import "./index.scss";
 
 const SearchPage = () => {
-  const { current } = useRef({ userInfo: null });
   const listViewRef = useRef(null);
-  const [seachType, setSeachType] = useState("kind");
-  const [addData, setAddData] = useState();
-  const [codeModal, setCodeModal] = useState(false);
+  const { current } = useRef({
+    infoData: "",
+  });
+  const [showData, setShowData] = useState();
 
-  const request = param => {
+  const requestList = (param) => {
     listViewRef.current.load({
       pn: param?.pn || 1,
-      ps: 10,
       ...param,
     });
   };
 
   useEffect(() => {
-    getStorageData("userInfo").then(values => {
+    getStorageData("userInfo").then((values) => {
       current.infoData = values;
-      request({ user_id: values?.user_id });
+      requestList({ user_id: values?.user_id });
     });
   }, []);
 
- 
-
-  const onItemGoods = values => {
-    // Taro.navigateTo({
-    //   url: `/pages/details/index?id=${values.batch_no}`
-    // });
-  };
-
-
-
-  const formSubmit = () => {
-    request({
-      user_id: current.infoData?.user_id,
-      kind: "",
-      date: ""
-    });
-  };
-
-  const renderList = data => {
+  const renderList = (data) => {
+    console.log("data renderList:>> ", data);
     const { records } = data;
-    console.log('renderList :>> ', data);
-    if (records && records.length === 0) {
+    if (isEmpty(data) || isEmpty(records)) {
+      console.log("暂无数据 :>> ", records);
       return <YNoData desc={"暂无数据"} />;
     }
     if (records && records.length > 0) {
-      return records.map(item => {
+      return records.map((item) => {
         return (
           <View
-            key={item.policy_id}
-            className="search_list-item"
+            key={item?.question_id}
+            className="list_list-item"
             onClick={() => {
-              onItemGoods(item);
+              setShowData(item);
             }}
           >
-            <View className="list-l-i-g">
-              <View className="list-l-i-g-top">批次id：</View>
-              <View className="list-l-i-g-top">{item?.title || "无"}</View>
+            <View className="list_list-item-cent">
+              <View className="list_list-item-cent-title"> {item.title}</View>
+              <View className="list_list-item-cent-left">
+                回复内容：  
+              </View>
+              <View className="list_list-item-cent-answer">
+                 {item.answer || "暂无回复"}
+              </View>
+              <View className="list_list-item-cent-info">
+                {item.create_time}
+              </View>
             </View>
-            <View className="list-l-i-g">
-              <View className="list-l-i-g-t">品种:</View>
-              <View className="list-l-i-g-v">{item?.tags || "无"}</View>
-            </View>
-            <View className="list-l-i-g">
-              <View className="list-l-i-g-t">现有规格:</View>
-              <View className="list-l-i-g-v">{item?.source || "无"}</View>
-            </View>
-            <View className="list-l-i-g">
-              <View className="list-l-i-g-t">临近时间:</View>
-              <View className="list-l-i-g-v">{item?.latest_time || 0}天</View>
-            </View>
-            <View className="list-l-i-g">
-              <View className="list-l-i-g-t">报损:</View>
-              <View className="list-l-i-g-v">{item?.damage || 0}</View>
-            </View>
-            <View className="list-l-i-g">
-              <View className="list-l-i-g-t">库存:</View>
-              <View className="list-l-i-g-v">{item?.stock || 0}</View>
-            </View>
-            <View className="list-l-i-g">
-              <View className="list-l-i-g-t">总占用空间:</View>
-              <View className="list-l-i-g-v">{item?.space || 0} 平方厘米</View>
+            <View className="list_list-item-img">
+              <Image
+                className="list_list-item-img-cent"
+                src={require("@/assets/xinxi_item.png")}
+              />
             </View>
           </View>
         );
       });
     }
-    return <YNoData desc={"加载中..."} />;
   };
-
   return (
     <View className="list">
-      {/* 搜索 */}
-      <View className="list-seach">
-          <YInputSearch />
-
-        <Button
-          type="primary"
-          className={"list-seach-button"}
-          onClick={formSubmit}
+      <View className="list_top">
+        <View className="list_top-item">
+          <Image
+            className="list_top-item-img"
+            src={require("@/assets/xinxi.png")}
+          />
+          <View  className="list_top-item-title">系统通知</View>
+        </View>
+        <View className="list_top-item">
+          <Image
+            className="list_top-item-img"
+            src={require("@/assets/xinxi_bug.png")}
+          />
+          <View className="list_top-item-title">问题反馈</View>
+        </View>
+        <View
+          className="list_top-item"
+          onClick={() => {
+            Taro.navigateTo({
+              url: `/pages/question/index`,
+              events: {
+                // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
+                QuestionPage: function () {
+                  requestList({ user_id: current.infoData?.user_id });
+                },
+              },
+            });
+          }}
         >
-          <View className="list-seach-button-c">搜索</View>
-        </Button>
+          <Image
+            className="list_top-item-img"
+            src={require("@/assets/xinxi_new.png")}
+          />
+          <View className="list_top-item-title">提问</View>
+        </View>
       </View>
-
-      {/* use-list */}
       <YListView
-        classStyle={"list-l"}
-        height={460}
-        pnParams
-        // boxHeight={140}
+        classStyle={"list_list"}
+        boxHeight={110}
         renderList={renderList}
         request={getList}
         ref={listViewRef}
+        extraParams={{}}
         manual
+        pnParams
       />
+      
     </View>
   );
 };
-
 export default SearchPage;
