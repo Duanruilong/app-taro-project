e08325cd96c28c6a0ab0c8c585ab29efbd9145ba
Drlong <drl1210@163.com>
2023-08-18 09:45:37 +0800

    try666

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -2,14 +2,14 @@
  * @Author: duanruilong
  * @Date: 2022-07-22 17:25:19
  * @LastEditors: Drlong drl1210@163.com
- * @LastEditTime: 2023-08-17 17:25:30
+ * @LastEditTime: 2023-08-18 09:44:29
  * @Description: 政策列表
  */
 import { useState, useRef, useEffect } from "react";
 import Taro from "@tarojs/taro";
-import { View, Input, Image } from "@tarojs/components";
+import { View, Button, Image } from "@tarojs/components";
 import YListView from "@/components/YListView";
-// import YInputSearch from "@/components/YInputSearch";
+import YInputSearch from "@/components/YInputSearch";
 import YNoData from "@/components/YNoData";
 import YButton from "@/components/YButton";
 import listItemImg from "@/assets/index_list1.png";
@@ -19,11 +19,13 @@ import { getList, getFollow } from "./service";
 import "./index.scss";
 
 const SearchPage = () => {
-  const { current } = useRef({ local: null });
+  const { current } = useRef({ userInfo: null });
   const listViewRef = useRef(null);
-  const { windowHeight } = Taro.getSystemInfoSync();
+  const [seachType, setSeachType] = useState("kind");
+  const [addData, setAddData] = useState();
+  const [codeModal, setCodeModal] = useState(false);
 
-  const requestList = (param) => {
+  const request = param => {
     listViewRef.current.load({
       pn: param?.pn || 1,
       ps: 10,
@@ -32,130 +34,106 @@ const SearchPage = () => {
   };
 
   useEffect(() => {
-    getStorageData("userInfo").then((values) => {
+    getStorageData("userInfo").then(values => {
       current.infoData = values;
-      requestList({ user_id: values?.user_id });
+      request({ user_id: values?.user_id });
     });
   }, []);
 
-  const onChange = (values) => {
-    console.log("onChange :>> ", values);
-    current.local = values;
+ 
+
+  const onItemGoods = values => {
+    // Taro.navigateTo({
+    //   url: `/pages/details/index?id=${values.batch_no}`
+    // });
   };
 
-  const onClearClick = (values) => {
-    console.log("onSearchChange :>> ", values);
-    current.local = null;
-    listViewRef.current.load({
+
+
+  const formSubmit = () => {
+    request({
       user_id: current.infoData?.user_id,
-      key: "",
-      pn: 1,
+      kind: "",
+      date: ""
     });
   };
 
-  const onSearchGoods = (values) => {
-    current.local = values;
-    console.log("onConfirmChange :>> ", values);
-    listViewRef.current.load({
-      user_id: current.infoData?.user_id,
-      key: values,
-      pn: 1,
-    });
-  };
-
-  const onConfirmChange = (values) => {
-    const searchValue = values.detail?.value || undefined;
-    onSearchGoods(searchValue);
-  };
-
-  const cliTip = (values) => {
-    getFollow({
-      user_id: current.infoData?.user_id,
-      policy_id: values?.policy_id,
-    })
-      .then(() => {
-        toast("关注该政策成功!");
-        onClearClick();
-      })
-      .catch(() => {});
-  };
-
-  const onEditData = async (values) => {
-    await Taro.setStorage({
-      key: "DAMAGE-ITEM",
-      data: values,
-    });
-    Taro.navigateTo({
-      url: `/pages/policyDetail/index`,
-    });
-  };
-
-  const renderList = (values) => {
-    const { records } = values;
-    if (isEmpty(values) || isEmpty(records)) {
+  const renderList = data => {
+    const { records } = data;
+    console.log('renderList :>> ', data);
+    if (records && records.length === 0) {
       return <YNoData desc={"暂无数据"} />;
     }
-    return records.map((item) => {
-      return (
-        <View
-          key={item?.policy_id}
-          className="search_list-item"
-          onClick={() => {
-            onEditData(item);
-          }}
-        >
-          <View className="search_list-item-cent">
-            <View className="search_list-item-cent-title">{item.title}</View>
-            {item?.tags && (
-              <View className="search_list-item-cent-tag">{item?.tags}</View>
-            )}
-            <View className="search_list-item-cent-info">
-              {item.create_time}
+    if (records && records.length > 0) {
+      return records.map(item => {
+        return (
+          <View
+            key={item.policy_id}
+            className="search_list-item"
+            onClick={() => {
+              onItemGoods(item);
+            }}
+          >
+            <View className="list-l-i-g">
+              <View className="list-l-i-g-top">批次id：</View>
+              <View className="list-l-i-g-top">{item?.title || "无"}</View>
             </View>
-
-            <View className="search_list-item-but">
-              <YButton
-                yType="default"
-                disabled={item?.follow === 1}
-                onClick={() => {
-                  cliTip(item);
-                }}
-              >
-                <View className="search_list-item-but-t">关注该政策</View>
-              </YButton>
+            <View className="list-l-i-g">
+              <View className="list-l-i-g-t">品种:</View>
+              <View className="list-l-i-g-v">{item?.tags || "无"}</View>
+            </View>
+            <View className="list-l-i-g">
+              <View className="list-l-i-g-t">现有规格:</View>
+              <View className="list-l-i-g-v">{item?.source || "无"}</View>
+            </View>
+            <View className="list-l-i-g">
+              <View className="list-l-i-g-t">临近时间:</View>
+              <View className="list-l-i-g-v">{item?.latest_time || 0}天</View>
+            </View>
+            <View className="list-l-i-g">
+              <View className="list-l-i-g-t">报损:</View>
+              <View className="list-l-i-g-v">{item?.damage || 0}</View>
+            </View>
+            <View className="list-l-i-g">
+              <View className="list-l-i-g-t">库存:</View>
+              <View className="list-l-i-g-v">{item?.stock || 0}</View>
+            </View>
+            <View className="list-l-i-g">
+              <View className="list-l-i-g-t">总占用空间:</View>
+              <View className="list-l-i-g-v">{item?.space || 0} 平方厘米</View>
             </View>
           </View>
-          <View className="search_list-item-img">
-            <Image className="search_list-item-img-cent" src={listItemImg} />
-          </View>
-        </View>
-      );
-    });
+        );
+      });
+    }
+    return <YNoData desc={"加载中..."} />;
   };
 
   return (
-    <View className="search">
-      {/* <View className="search_top">
-        <View className="search_top-cent">
-          <YInputSearch
-            className={"search_top-input"}
-            placeholder={"搜索最新政策"}
-            clear
-            onClearClick={onClearClick}
-            onConfirm={onConfirmChange}
-            onChange={onChange}
-          />
-        </View>
-      </View> */}
+    <View className="list">
+      {/* 搜索 */}
+      <View className="list-seach">
+          <YInputSearch />
+
+        <Button
+          type="primary"
+          className={"list-seach-button"}
+          onClick={formSubmit}
+        >
+          <View className="list-seach-button-c">搜索</View>
+        </Button>
+      </View>
+
+      {/* use-list */}
       <YListView
-        classStyle="search_list"
-        ref={listViewRef}
-        boxHeight={230}
+        classStyle={"list-l"}
+        height={460}
+        pnParams
+        // boxHeight={140}
         renderList={renderList}
         request={getList}
-        extraParams={{}}
+        ref={listViewRef}
         manual
-        pnParams
       />
     </View>
   );
