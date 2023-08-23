a691309082686473323218115e20345278c4709c
Drlong <drl1210@163.com>
2023-08-23 16:42:45 +0800

    navigationBarBackgroundColor

--- src/pages/user/index.config.js
+++ src/pages/user/index.config.js
@@ -2,14 +2,14 @@
  * @Author: Drlong drl1210@163.com
  * @Date: 2023-08-09 15:27:56
  * @LastEditors: Drlong drl1210@163.com
- * @LastEditTime: 2023-08-16 17:22:50
+ * @LastEditTime: 2023-08-23 16:42:39
  * @FilePath: \app-taro-project\src\pages\search\index.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 /* eslint-disable no-undef */
 export default definePageConfig({
   navigationBarTitleText: '我的',
-  navigationBarBackgroundColor: '#ffffff',
+  navigationBarBackgroundColor: '#05aa9c',
   navigationBarTextStyle: 'white',
   // 当 `onShareAppMessage` 没有触发时，可以尝试配置此选项
   enableShareAppMessage: true,
